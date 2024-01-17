b8961e87b087dab366e8296fd698d23684c8c8ca
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-01-17 12:40:14 -0700

    add hugging face functions

--- load_hugging_face.py
+++ load_hugging_face.py
@@ -1,2 +1,29 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+def create_pipe(model_path:str, cache_model:bool=False, path_to_save:str=""):
+    """
+    Download the model from hugging face and return a pipeline object
+    """
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+
+    if cache_model:
+        if path_to_save=="":
+            path_to_save = model_path
+        model.save_pretrained(path_to_save)
+
+    pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+    return pipe
+
+
+# Sample Usage
+model_path = "distilgpt2"
+pipe = create_pipe(model_path=model_path)# cache_model=True, path_to_save="models/"+model_name)
+prompts: list[str] = ["Hello, my name is", "In 1776, the United States of America"]
+generated_response: list[list[dict[str:str]]] = pipe(prompts)
+
+# print((generated_response))
+# print((generated_response[0][0]['generated_text']))
+
+print([generated_response[i][0]['generated_text'] for i in range(len(prompts))])
 
